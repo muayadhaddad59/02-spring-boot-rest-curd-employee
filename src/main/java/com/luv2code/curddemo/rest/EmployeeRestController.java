cb94fb7d9c99ac8c18127f0ff58e09b4e6a121aa
Muayad Haddad <74570275+muayadhaddad59@users.noreply.github.com>
2026-09-16 21:14:17 -0400

    feat: get employee by id

--- src/main/java/com/luv2code/curddemo/rest/EmployeeRestController.java
+++ src/main/java/com/luv2code/curddemo/rest/EmployeeRestController.java
@@ -4,6 +4,7 @@ import com.luv2code.curddemo.entity.Employee;
 import com.luv2code.curddemo.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,6 +26,18 @@ public class EmployeeRestController {
     @GetMapping("/employees")
     public List<Employee> findAll() {
         return employeeService.findAll();
+    }
+
+    // Add mapping for GET /employees/{employeeId}
+    @GetMapping("/employee/{employeeId}")
+    public Employee getEmployee(@PathVariable int employeeId){
+        Employee theEmployee = employeeService.findById(employeeId);
+
+        if(theEmployee == null) {
+            throw new RuntimeException("Employee id not found - " + employeeId);
+        }
+
+        return theEmployee;
     }
 
 }
